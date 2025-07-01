35249de9b88322563a5115f6524db60452660807
Mara <peter.mara@outlook.de>
2025-07-01 23:18:55 +0200

    kommentiert

--- Seminar_Rcode.R
+++ Seminar_Rcode.R
@@ -1,6 +1,8 @@
 # -----------------------------------------------------------------------------
+#
 # Comparison of Cross Validation and Bootstrap
 # R-code
+#
 # -----------------------------------------------------------------------------
 
 # Empty the working dictionary
@@ -11,9 +13,9 @@ library("Metrics")
 library("cv")
 
 
-###############################################################################
-### Self-written Functions
-###############################################################################
+# -----------------------------------------------------------------------------
+# Self-written Functions
+# -----------------------------------------------------------------------------
 
 simulation <- function(Laenge, k, 
                             X.means = c(1, 1, 1, 1, 1, 1, 1), 
@@ -214,24 +216,30 @@ bootstrapPE <- function(data, B = 30, estimator = 3, linear = TRUE) {
 
 
 
-###############################################################################
-### Simulation
-###############################################################################
+# -----------------------------------------------------------------------------
+# Simulation
+# -----------------------------------------------------------------------------
+
+# fix a seed
 seed <- 1234
 
 # sample size
-n.size <- seq(20, 60)
+# it valid to have a small sample size
+n.size <- seq(100, 120)
+
 # number of covaraites
 k <- 5
 
-
-# how many replications on the same dataset?
+# Bootstrap and Cross Validation are fit repeatedly to to calculate 
+# the mean and variance of the methods
 howoften <- 30
 
-
+###############################################################################
 ### linear model
+###############################################################################
 
 {
+# initiate vectors to save the values later
 # mean
 m.BE1 <- numeric(0)
 m.BE2 <- numeric(0)
@@ -239,7 +247,6 @@ m.BE3 <- numeric(0)
 m.CV10 <- numeric(0)
 m.CVn <- numeric(0)
 m.actual <- numeric(0)
-
 # Variance
 v.BE1 <- numeric(0)
 v.BE2 <- numeric(0)
@@ -247,8 +254,6 @@ v.BE3 <- numeric(0)
 v.CV10 <- numeric(0)
 v.CVn <- numeric(0)
 v.actual <- numeric(0)
-
-
 # measure the time
 time1 <- numeric(0)
 time2 <- numeric(0)
@@ -257,52 +262,52 @@ time4 <- numeric(0)
 time5 <- numeric(0)
 time6 <- numeric(0)
 
-
-
-
-
+# set the seed
 set.seed(seed)
+# simulate the data
 simsi <- simulation(max(n.size), k = k)
 
+# for different sample sizes
 for (num in n.size) {
   
-  # have nested models
+  # for every sample size enlarge the data set 
   simsala <- list(simsi[[1]][1:num,], simsi[[2]][1:num])
   
-  # apply bootstrap and CV to each simulation
+  # initiate vectors to save the values later
   BE1 <- numeric(0)
   BE2 <- numeric(0)
   BE3 <- numeric(0)
-  actual <- numeric(0)
   CV10 <- numeric(0)
   CVn <- numeric(0)
+  actual <- numeric(0)
   
-  
+  ### use every method to calculate the prediction error -howoften- times
   time1 <- c(time1, system.time(
   for (o in 1:howoften) {
     BE1 <- c(BE1, bootstrapPE(simsala[[1]], estimator = 1))
   }
-  ) [1])
+  ) [1] / howoften) # find out how long -howoften- calculations of the 
+                    # prediction error take and devide by howoften
   
   time2 <- c(time2, system.time(
   for (o in 1:howoften) {
     BE2 <- c(BE2, bootstrapPE(simsala[[1]], estimator = 2))
   }
-  ) [1])
+  ) [1] / howoften)
   
   time3 <- c(time3, system.time(
   for (o in 1:howoften) {
     BE3 <- c(BE3, bootstrapPE(simsala[[1]], estimator = 3))
   }
-  ) [1])
+  ) [1] / howoften)
   
   time4 <- c(time4, system.time(
   for (o in 1:howoften) {
     
     lm1 <- lm(y ~., data = simsala[[1]])
-    CV10 <- c(CV10, cv(lm1)$`CV crit`[[1]])  
+    CV10 <- c(CV10, cv(lm1)$`CV crit`[[1]])  # cv() 
   }
-  ) [1])
+  ) [1] / howoften)
   
   time5 <- c(time5, system.time(
   for (o in 1:howoften) {
@@ -310,15 +315,16 @@ for (num in n.size) {
     lm1 <- lm(y ~., data = simsala[[1]])
     CVn <- c(CVn, cv(lm1, k = "loo")$`CV crit`[[1]])
   }
-  ) [1])
+  ) [1] / howoften)
   
+  # the actual prediction error
   time6 <- c(time6, system.time(
     for (o in 1:howoften) {
       actual <- c(actual, mean(simsala[[2]]^2))
     }
-  ) [1])
+  ) [1] / howoften)
   
-  
+  ### mean and variance of each method
   m.BE1 <- c(m.BE1, mean(BE1))
   m.BE2 <- c(m.BE2, mean(BE2))
   m.BE3 <- c(m.BE3, mean(BE3))
@@ -333,65 +339,61 @@ for (num in n.size) {
   v.CVn <- c(v.CVn, var(CVn))
   v.actual <- c(v.actual, var(actual))
 
-
 }
 
 
 
-# plot
-nnn <- n.size[1:length(m.BE1)]
+### plot the results
 
+# delimiters of the plot windows
 limits.m <- c(min(m.BE1, m.BE2, m.BE3, m.CV10, m.CVn, m.actual), 
               max(m.BE1, m.BE2, m.BE3, m.CV10, m.CVn, m.actual))
 limits.v <- c(min(v.BE1, v.BE2, v.BE3, v.CV10, v.CVn, v.actual), 
             max(v.BE1, v.BE2, v.BE3, v.CV10, v.CVn, v.actual))
-
 limits.time <- c(min(time1, time2, time3, time4, time5, time6), 
               max(time1, time2, time3, time4, time5, time6))
-
+# 
 lwd <- 2
 
-plot(nnn, m.actual, type = "l", 
+# mean
+plot(n.size, m.actual, type = "l", 
      main = "", 
      col = "black", 
      lwd = lwd, 
      ylim = limits.m,
      xlab = "sample size", ylab = "Mean of the prediction error")
-lines(nnn, m.BE1, col = "lightgreen", lwd = lwd)
-lines(nnn, m.BE2, col = "seagreen", lwd = lwd, lty = 2)
-lines(nnn, m.BE3, col = "darkgreen", lwd = lwd, lty = 3)
-lines(nnn, m.CV10, col = "firebrick", lwd = lwd)
-lines(nnn, m.CVn, col = "darkred", lwd = lwd, lty = 2)
+lines(n.size, m.BE1, col = "lightgreen", lwd = lwd)
+lines(n.size, m.BE2, col = "seagreen", lwd = lwd, lty = 2)
+lines(n.size, m.BE3, col = "darkgreen", lwd = lwd, lty = 3)
+lines(n.size, m.CV10, col = "firebrick", lwd = lwd)
+lines(n.size, m.CVn, col = "darkred", lwd = lwd, lty = 2)
 
-
-plot(nnn, sqrt(v.actual), type = "l", 
+# varianve
+plot(n.size, v.actual, type = "l", 
      main = "", 
      col = "black", 
      lwd = lwd, 
      ylim = limits.v,
      xlab = "sample size", ylab = "Variance of the prediction error")
-lines(nnn, sqrt(v.BE1), col = "lightgreen", lwd = lwd)
-lines(nnn, sqrt(v.BE2), col = "seagreen", lwd = lwd, lty = 2)
-lines(nnn, sqrt(v.BE3), col = "darkgreen", lwd = lwd, lty = 3)
-lines(nnn, sqrt(v.CV10), col = "firebrick", lwd = lwd)
-lines(nnn, sqrt(v.CVn), col = "darkred", lwd = lwd, lty = 2)
+lines(n.size, v.BE1, col = "lightgreen", lwd = lwd)
+lines(n.size, v.BE2, col = "seagreen", lwd = lwd, lty = 2)
+lines(n.size, v.BE3, col = "darkgreen", lwd = lwd, lty = 3)
+lines(n.size, v.CV10, col = "firebrick", lwd = lwd)
+lines(n.size, v.CVn, col = "darkred", lwd = lwd, lty = 2)
 
 
-
-
-plot(nnn, time6, type = "l", 
+# CPU time
+plot(n.size, time6, type = "l", 
      main = "", 
      col = "black", 
      lwd = lwd, 
      ylim = limits.time,
-     xlab = "sample size", ylab = paste("CPU time of the method", 
-                                        howoften, 
-                                        "times in seconds"))
-lines(nnn, time1, col = "lightgreen", lwd = lwd)
-lines(nnn, time2, col = "seagreen", lwd = lwd, lty = 2)
-lines(nnn, time3, col = "darkgreen", lwd = lwd, tlty = 3)
-lines(nnn, time4, col = "firebrick", lwd = lwd)
-lines(nnn, time5, col = "darkred", lwd = lwd, lty = 2)
+     xlab = "sample size", ylab = "CPU time of the method times in seconds")
+lines(n.size, time1, col = "lightgreen", lwd = lwd)
+lines(n.size, time2, col = "seagreen", lwd = lwd, lty = 2)
+lines(n.size, time3, col = "darkgreen", lwd = lwd, lty = 3)
+lines(n.size, time4, col = "firebrick", lwd = lwd)
+lines(n.size, time5, col = "darkred", lwd = lwd, lty = 2)
 
 }
 
@@ -399,10 +401,12 @@ lines(nnn, time5, col = "darkred", lwd = lwd, lty = 2)
 
 
 
-
+###############################################################################
 ### Classification
+###############################################################################
 
-
+# to calculate the total prediction error in a 0,1 classification 
+# is equivalent to the mse
 
 
 
@@ -515,8 +519,8 @@ lines(nnn, time5, col = "darkred", lwd = lwd, lty = 2)
   
   
   
-  # plot
-  nnn <- n.size[1:length(m.BE1)]
+  ### plot
+  n.size <- n.size[1:length(m.BE1)]
   
   limits.m <- c(min(m.BE1, m.BE2, m.BE3, m.CV10, m.CVn, m.actual), 
                 max(m.BE1, m.BE2, m.BE3, m.CV10, m.CVn, m.actual))
@@ -528,35 +532,35 @@ lines(nnn, time5, col = "darkred", lwd = lwd, lty = 2)
   
   lwd <- 2
   
-  plot(nnn, m.actual, type = "l", 
+  plot(n.size, m.actual, type = "l", 
        main = "", 
        col = "black", 
        lwd = lwd, 
        ylim = limits.m,
        xlab = "sample size", ylab = "Mean of the prediction error")
-  lines(nnn, m.BE1, col = "lightgreen", lwd = lwd)
-  lines(nnn, m.BE2, col = "seagreen", lwd = lwd, lty = 2)
-  lines(nnn, m.BE3, col = "darkgreen", lwd = lwd, lty = 3)
-  lines(nnn, m.CV10, col = "firebrick", lwd = lwd)
-  lines(nnn, m.CVn, col = "darkred", lwd = lwd, lty = 2)
+  lines(n.size, m.BE1, col = "lightgreen", lwd = lwd)
+  lines(n.size, m.BE2, col = "seagreen", lwd = lwd, lty = 2)
+  lines(n.size, m.BE3, col = "darkgreen", lwd = lwd, lty = 3)
+  lines(n.size, m.CV10, col = "firebrick", lwd = lwd)
+  lines(n.size, m.CVn, col = "darkred", lwd = lwd, lty = 2)
   
   
-  plot(nnn, sqrt(v.actual), type = "l", 
+  plot(n.size, sqrt(v.actual), type = "l", 
        main = "", 
        col = "black", 
        lwd = lwd, 
        ylim = limits.v,
        xlab = "sample size", ylab = "Variance of the prediction error")
-  lines(nnn, sqrt(v.BE1), col = "lightgreen", lwd = lwd)
-  lines(nnn, sqrt(v.BE2), col = "seagreen", lwd = lwd, lty = 2)
-  lines(nnn, sqrt(v.BE3), col = "darkgreen", lwd = lwd, lty = 3)
-  lines(nnn, sqrt(v.CV10), col = "firebrick", lwd = lwd)
-  lines(nnn, sqrt(v.CVn), col = "darkred", lwd = lwd, lty = 2)
+  lines(n.size, sqrt(v.BE1), col = "lightgreen", lwd = lwd)
+  lines(n.size, sqrt(v.BE2), col = "seagreen", lwd = lwd, lty = 2)
+  lines(n.size, sqrt(v.BE3), col = "darkgreen", lwd = lwd, lty = 3)
+  lines(n.size, sqrt(v.CV10), col = "firebrick", lwd = lwd)
+  lines(n.size, sqrt(v.CVn), col = "darkred", lwd = lwd, lty = 2)
   
   
   
   
-  plot(nnn, time6, type = "l", 
+  plot(n.size, time6, type = "l", 
        main = "", 
        col = "black", 
        lwd = lwd, 
@@ -564,11 +568,11 @@ lines(nnn, time5, col = "darkred", lwd = lwd, lty = 2)
        xlab = "sample size", ylab = paste("CPU time of the method", 
                                           howoften, 
                                           "times in seconds"))
-  lines(nnn, time1, col = "lightgreen", lwd = lwd)
-  lines(nnn, time2, col = "seagreen", lwd = lwd, lty = 2)
-  lines(nnn, time3, col = "darkgreen", lwd = lwd, tlty = 3)
-  lines(nnn, time4, col = "firebrick", lwd = lwd)
-  lines(nnn, time5, col = "darkred", lwd = lwd, lty = 2)
+  lines(n.size, time1, col = "lightgreen", lwd = lwd)
+  lines(n.size, time2, col = "seagreen", lwd = lwd, lty = 2)
+  lines(n.size, time3, col = "darkgreen", lwd = lwd, tlty = 3)
+  lines(n.size, time4, col = "firebrick", lwd = lwd)
+  lines(n.size, time5, col = "darkred", lwd = lwd, lty = 2)
   
 }
 
